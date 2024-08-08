f58ae3b01ca24aa577bbf4a7c967c1f211178c71
Pavlo Holovachov <adamdoom23@gmail.com>
2024-08-08 11:59:40 +0300

    Updated the list tile

--- components/ListTile/index.tsx
+++ components/ListTile/index.tsx
@@ -1,17 +1,36 @@
-import { FC } from 'react';
-import { useColorScheme, View } from 'react-native';
+import { FC, useCallback, useState } from 'react';
+import { Pressable, useColorScheme, View } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 
 import { Colors } from '@/constants/Colors';
 import { ThemedText } from '@/components/ThemedText';
+import ConfirmationModal from '@/components/ConfirmationModal';
+import LocationEntity from '@/entities/locationsEntity';
+import { useAppDispatch } from '@/hooks/useStore';
+import { locationsActions } from '@/store/locations/slice';
 import styles from './styles';
 
 interface Props {
-  id: number;
+  location: LocationEntity;
 }
 
-const ListTile: FC<Props> = ({ id }) => {
+const ListTile: FC<Props> = ({ location: { name, id } }) => {
   const colorScheme = useColorScheme();
+  const dispatch = useAppDispatch();
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleConfirm = useCallback(
+    (id: string) => {
+      dispatch(locationsActions.removeLocation(id));
+      setModalVisible(false);
+    },
+    [dispatch],
+  );
+
+  const handleCancel = () => setModalVisible(false);
+
+  const handleOpenModal = () => setModalVisible(true);
+
   return (
     <View
       style={[
@@ -20,12 +39,20 @@ const ListTile: FC<Props> = ({ id }) => {
       ]}
     >
       <View style={styles.tileName}>
-        <ThemedText numberOfLines={1}>Some place {`num ${id}`}</ThemedText>
+        <ThemedText numberOfLines={1}>{name}</ThemedText>
       </View>
       <View style={styles.tileActions}>
         <AntDesign name="infocirlceo" size={24} color={Colors.light.primary} />
-        <AntDesign name="delete" size={24} color="red" />
+        <Pressable onPress={handleOpenModal}>
+          <AntDesign name="delete" size={24} color="red" />
+        </Pressable>
       </View>
+      <ConfirmationModal
+        visible={modalVisible}
+        title="Are you sure, you want to delete this location?"
+        onConfirm={() => handleConfirm(id)}
+        onCancel={handleCancel}
+      />
     </View>
   );
 };
