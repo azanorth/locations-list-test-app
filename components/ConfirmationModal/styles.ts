2c68bb1f0d78560845f1c2976437ccfd3cde534f
Pavlo Holovachov <adamdoom23@gmail.com>
2024-08-08 12:00:13 +0300

    Added the confirmation modal component

---
+++ components/ConfirmationModal/styles.ts
@@ -0,0 +1,53 @@
+import { Colors } from '@/constants/Colors';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalView: {
+    margin: 20,
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 20,
+  },
+  button: {
+    elevation: 2,
+    marginHorizontal: 10,
+    backgroundColor: Colors.light.primary,
+    height: 40,
+    width: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 5,
+    borderRadius: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalTitle: {
+    marginBottom: 15,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
+
+export default styles;
